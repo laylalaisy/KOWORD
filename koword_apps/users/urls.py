274743908b90ab782a54b12301995f4c5dfa56c9
laylalaisy <lsylayla@163.com>
2018-06-28 04:45:26 -0700

    add koword_apps

--- koword_apps/users/urls.py
+++ koword_apps/users/urls.py
@@ -18,6 +18,5 @@ from django.conf.urls import url
 from .views import LoginView
 
 urlpatterns = [
-    # url(r'^admin/', admin.site.urls),
     url(r'^login/$', LoginView.as_view(), name='login'),
 ]
