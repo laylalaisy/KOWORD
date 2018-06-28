9e93ba31b3ba7233c00807a1225953ad7bed9bd4
laylalaisy <lsylayla@163.com>
2018-06-28 06:05:19 -0700

    modify login

--- koword_apps/users/forms.py
+++ koword_apps/users/forms.py
@@ -3,8 +3,6 @@ import json
 
 from django import forms
 
-from .models import UserProfile
-
 class LoginForm(forms.Form):
 	username = forms.CharField(required=True, min_length=6, max_length=20)
 	password = forms.CharField(required=True, min_length=6, max_length=200)
--- koword_apps/users/views.py
+++ koword_apps/users/views.py
@@ -8,14 +8,29 @@ from django.db.models import Q
 from .forms import LoginForm
 from .models import UserProfile
 
-class CustomBackend(ModelBackend):
+# -*- coding: utf-8 -*-
+import json
 
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.urlresolvers import reverse
+from django.db.models import Q
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse, Http404
+from django.views.generic import View
+#from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+from django.conf import settings
+
+class CustomBackend(ModelBackend):
 	def authenticate(self, username=None, password=None, **kwargs):
 		'''
 		authenticate if user is valid to login
 		'''
 		try:
-			user = UserProfile.objects.get(Q(username=username) | Q(email=username))
+			user = UserProfile.objects.get(Q(username=username)|Q(email=username))
 			if user.check_password(password): 	# password stored in backend is encrypted
 				return user 					# success to login
 		except Exception as e:
@@ -33,9 +48,9 @@ class LoginView(View):
     		user = authenticate(username=username_, password=password_)	
     		if user is not None:				# user is valid
     			login(request, user)	
-    			return render(reverse("index.html"))	# success to login
+    			return render(reverse("index.html"))	# success to login}
     		else:
-    			return render(request, "login.html", {"msg": u"Username or password is wrong!"})	# fail to login
+    			return render(request, "login.html", {"msg": u"Username or password is wrong!", "login_form": login_form})	# fail to login
     	else:
-            return render(request, "login.html", {"login_form": login_form})	# input is not valid
+            return render(request, "login.html", {"msg": u"Username or password should be longer than 6 characters or numbers!", "login_form": login_form}) # input is not valid
 
